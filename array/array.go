236bb8684a232ab6264e9b80804bda235a48f9d7
Devara <adityadevara91@gmail.com>
2024-09-26 01:57:11 +0700

    feat(array): add append unique and update existing
    add append unique function and change existing function to accept any type

--- array/array.go
+++ array/array.go
@@ -1,10 +1,28 @@
 package array
 
 // Reverse array of string
-func Reverse(arr []string) []string {
+func Reverse[T comparable](arr []T) []T {
 	for i := 0; i < len(arr)/2; i++ {
 		j := len(arr) - i - 1
 		arr[i], arr[j] = arr[j], arr[i]
 	}
 	return arr
+}
+
+func AppendUnique[T comparable](dest []T, src ...T) []T {
+	exists := make(map[T]bool)
+	// Mark existing elements from dest in the map
+	for _, v := range dest {
+		exists[v] = true
+	}
+
+	// Only append elements from src that don't already exist
+	for _, v := range src {
+		if !exists[v] {
+			dest = append(dest, v)
+			exists[v] = true
+		}
+	}
+
+	return dest
 }
